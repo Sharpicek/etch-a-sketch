c3fff0bc303f2b831be6d8b53ef2b2da58847a3b
Sharpicek <davidthomas13@proton.me>
2024-08-10 15:22:01 +0200

    Update for loop for creating boxes

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ let arrayBox = [];
 for (let i = 0; i < 256; i++) {
   const singleBox = document.createElement("div");
   singleBox.classList.add("inner-box", "box" + (i + 1));  
-  arrayBox.push(singleBox);
-  const innerBox = arrayBox[i];
-  container.append(innerBox);
-  console.log(innerBox);
+  arrayBox.push(singleBox); // maybe I will need and array of the boxes
+  container.appendChild(singleBox);
 }
-console.log(arrayBox);
+
