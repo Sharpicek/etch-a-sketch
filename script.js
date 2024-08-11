fd64275dbabee19fc9fce60a51f3b69b0f5c4984
Sharpicek <davidthomas13@proton.me>
2024-08-11 14:43:52 +0200

    Function getGridSize fills the grid based on input

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.getElementById("container");
 
-function getGridSize(userInput = 16) {
-  const numberOfSquares = userInput * userInput;
+function getGridSize(squaresInput = 16) {
+  const numberOfSquares = squaresInput * squaresInput;
+  const flexBasisPercent = 100 / squaresInput;
+  console.log(flexBasisPercent); 
   for (let i = 0; i < numberOfSquares; i++) {
     const singleSquare = document.createElement("div");
+    singleSquare.setAttribute("style", `flex-basis: ${flexBasisPercent}%`);
     singleSquare.classList.add("inner-box", "box" + (i + 1));
     singleSquare.addEventListener("mouseover", () => {
       coloring(singleSquare);
