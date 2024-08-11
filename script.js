1685ac2232391b88e89a36c8f7df255cd7181a1a
Sharpicek <davidthomas13@proton.me>
2024-08-11 15:47:30 +0200

    Create setSizeButton action
    This deletes previous grid and creates new one base on user input.

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const container = document.getElementById("container");
+const setSizeButton = document.getElementById("set-size");
+let previousGridSize = 0;
+
+setSizeButton.onclick = () => {
+    const setSizeValue = document.getElementById("grid-size").value;
+    getGridSize(setSizeValue);
+    console.log(setSizeValue);
+};
 
 function getGridSize(squaresInput = 16) {
   const numberOfSquares = squaresInput * squaresInput;
+
+  for (let i = 0; i < previousGridSize; i++) {
+    const singleSquare = document.querySelector(".inner-box");
+    container.removeChild(singleSquare);
+  }
+
+  previousGridSize = numberOfSquares;
   const flexBasisPercent = 100 / squaresInput;
-  console.log(flexBasisPercent); 
+
   for (let i = 0; i < numberOfSquares; i++) {
     const singleSquare = document.createElement("div");
     singleSquare.setAttribute("style", `flex-basis: ${flexBasisPercent}%`);
@@ -13,10 +28,9 @@ function getGridSize(squaresInput = 16) {
     });
     container.appendChild(singleSquare);
   }
-};
+}
 
 function coloring(listener) {
   listener.style.backgroundColor = "red";
-};
-
+}
 getGridSize();
