0a3377f95b419bdce5467e1a4c8df469b23e8951
Sharpicek <davidthomas13@proton.me>
2024-08-12 13:23:19 +0200

    Create working color scheme selector

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const container = document.getElementById("container");
-const setSizeButton = document.getElementById("set-size")
+const setSizeButton = document.getElementById("set-size");
 let previousGridSize = 0;
+let rainbowCounter = 0;
 
 setSizeButton.onclick = () => {
   const setSizeValue = document.getElementById("grid-size").value;
   getGridSize(setSizeValue);
 };
 
-const resetGridByPressingEnter = document.getElementById("grid-size").onkeydown = (event) => {
+const resetGridByPressingEnter = (document.getElementById(
+  "grid-size"
+).onkeydown = (event) => {
   if (event.key === "Enter") {
     getGridSize(document.getElementById("grid-size").value);
   }
-};
-
+});
 
 function getGridSize(squaresInput = 16) {
   if (parseInt(squaresInput) === NaN || squaresInput <= 0) {
@@ -39,16 +41,33 @@ function getGridSize(squaresInput = 16) {
     });
     container.appendChild(singleSquare);
   }
-};
+}
 
 function coloring(listener) {
   let recoloringCheckbox = document.getElementById("recoloring").checked;
-  if (listener.style.backgroundColor !== "" && !recoloringCheckbox) {
+  if ((listener.style.backgroundColor !== "") && !recoloringCheckbox) {
     return;
   }
-  let randomRed = Math.floor(Math.random() * 255);
-  let randomGreen = Math.floor(Math.random() * 255);
-  let randomBlue = Math.floor(Math.random() * 255);
-  listener.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+  let colorScheme = document.getElementById("color-scheme").value;
+  switch (parseInt(colorScheme)) {
+    case 1:
+      listener.style.backgroundColor = "orange";
+      break;
+    case 2:
+      listener.style.backgroundColor = "grey";
+      listener.classList.add = "grey";
+      break;
+    case 3:
+      let randomRed = Math.floor(Math.random() * 255);
+      let randomGreen = Math.floor(Math.random() * 255);
+      let randomBlue = Math.floor(Math.random() * 255);
+      listener.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+      break;
+    case 4:
+      const rainbow = ["#E81416", "#FFA500", "#FAEB36", "#79C314", "#487DE7", "#4B369D", "#70369D"];
+      listener.style.backgroundColor = rainbow[rainbowCounter];
+      rainbowCounter == 6 ? rainbowCounter = 0 : rainbowCounter++;
+      break;
+  }
 }
 getGridSize();
